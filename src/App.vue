3e9952b68a2e2ca88bcfff217fb1cc92043c5c45
fexcode <2734664632@qq.com>
2025-06-02 12:56:17 +0800

    添加访问次数显示&访问次数增加&服务器信息获取缓存功能

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
 import { RouterView } from "vue-router";
-import { watch, ref } from "vue";
+import { watch, ref, onMounted } from "vue";
 import blogs from "./blogs.js";
 import "sober";
+import axios from "axios";
 
+const serverApiUrl = "https://xnors.pythonanywhere.com/get_visit_count";
+const sendNewVisitToServer = () => {
+  axios
+    .get(serverApiUrl)
+    .then((response) => {
+      console.log(response.data);
+      if (response.data != "ok") {
+        console.log("error", response.data);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 let searchText = ref("");
 
 // 监听 searchText 的变化并打印其值
@@ -15,8 +30,6 @@ watch(searchText, (newValue) => {
 
 const blogsFiltered = ref(blogs);
 
-
-
 // 过滤博客列表
 function searchBlogs(text) {
   blogsFiltered.value = blogs.filter(
@@ -26,6 +39,10 @@ function searchBlogs(text) {
   );
   console.log(blogsFiltered.value);
 }
+
+onMounted(() => {
+  sendNewVisitToServer();
+});
 </script>
 
 <template>
@@ -40,7 +57,34 @@ function searchBlogs(text) {
         <div class="headline">XBlogs</div>
       </div>
 
-      <input placeholder="搜索博客" class="search" v-model="searchText" />
+      <div class="appber-left">
+        <input placeholder="搜索博客" class="search" v-model="searchText" />
+        <s-dialog class="dialog-box">
+          <s-icon-button slot="trigger">
+            <s-icon>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="128"
+                height="128"
+                viewBox="0 0 64 64"
+              >
+                <path
+                  d="M 32 10 C 19.85 10 10 19.85 10 32 C 10 44.15 19.85 54 32 54 C 44.15 54 54 44.15 54 32 C 54 19.85 44.15 10 32 10 z M 32 14 C 41.941 14 50 22.059 50 32 C 50 41.941 41.941 50 32 50 C 22.059 50 14 41.941 14 32 C 14 22.059 22.059 14 32 14 z M 32 21 C 30.343 21 29 22.343 29 24 C 29 25.657 30.343 27 32 27 C 33.657 27 35 25.657 35 24 C 35 22.343 33.657 21 32 21 z M 32 30 C 30.895 30 30 30.896 30 32 L 30 42 C 30 43.104 30.895 44 32 44 C 33.105 44 34 43.104 34 42 L 34 32 C 34 30.896 33.105 30 32 30 z"
+                ></path>
+              </svg>
+            </s-icon>
+          </s-icon-button>
+          <div slot="headline">Title</div>
+          <div slot="text">
+            问人生、头白京国，算来何事消得。不如罨画清溪上，蓑笠扁舟一只。人不识，且笑煮、鲈鱼趁著莼丝碧。无端酸鼻，向岐路消魂，征轮驿骑，断雁西风急。
+            英雄辈，事业东西南北。临风因甚泣。酬知有愿频挥手，零雨凄其此日。休太息，须信道、诸公衮衮皆虚掷。年来踪迹。有多少雄心，几翻恶梦，泪点霜华织。
+          </div>
+          <s-button slot="action" type="text">取消</s-button>
+          <s-button slot="action" type="text">确定</s-button>
+        </s-dialog>
+      </div>
     </div>
     <main>
       <RouterView :blogs="blogsFiltered" />
@@ -64,34 +108,55 @@ $background-color: #1c1c1d; // 修正拼写错误
     margin-top: 3px;
     font-weight: bold;
   }
-}
+  .appber-left {
+    width: 60%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: right;
+    height: 100%;
 
-.search {
-  background-color: #313133;
-  box-shadow: none;
-  border: none;
-  padding: calc(0.5vw + 0.6vh);
-  margin-right: 2vw;
-  color: white;
-  border-radius: 3px;
-  height: 60%;
-  min-width: 20%;
-  width: 20%;
-  outline: none;
-  transition: all 0.3s ease;
-  &:hover {
-    background-color: #414144;
-  }
-  &:focus {
-    width: 50%;
-    background-color: #38383b;
+    .search {
+      background-color: #313133;
+      box-shadow: none;
+      border: none;
+      padding: calc(0.5vw + 0.6vh);
+      margin-right: 1vw;
+      color: white;
+      border-radius: 3px;
+      height: 60%;
+      min-width: 20%;
+      width: 30%;
+      outline: none;
+      transition: all 0.3s ease;
+      &:hover {
+        background-color: #414144;
+      }
+      &:focus {
+        width: 80%;
+        background-color: #38383b;
+      }
+    }
+    .dialog-box {
+      div[slot="text"] {
+        // display: none;
+        margin: 1vh 1.6vw;
+      }
+
+      div[slot="headline"] {
+        margin: 3vh 1.6vw;
+        margin-bottom: 0.2vh;
+      }
+    }
   }
 }
 
 @media screen and (max-width: 768px) {
   .search {
-    width: 30%;
-    min-width: 30%;
+    width: 50%;
+    & :focus {
+      width: 80%;
+    }
   }
 }
 </style>
--- src/blogs/Welcome.vue
+++ src/blogs/Welcome.vue
@@ -1,11 +1,61 @@
 <script setup>
-import "sober";
 import { marked } from "marked";
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+const visitCount = ref("(正在获取...)");
+const serverApiUrl = "https://xnors.pythonanywhere.com/get_visit_count";
+
+const getVisitCount = async () => {
+  console.log("Getting visit count from server...");
+  try {
+    const response = await axios.get(serverApiUrl);
+    const count = response.data;
+    const timestamp = Date.now();
+
+    // 保存到 localStorage
+    localStorage.setItem("visitCount", count);
+    localStorage.setItem("timestamp", timestamp);
+
+    visitCount.value = count;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const loadFromCache = () => {
+  const cachedCount = localStorage.getItem("visitCount");
+  const cachedTimestamp = localStorage.getItem("timestamp");
+
+  if (cachedCount && cachedTimestamp) {
+    const currentTime = Date.now();
+    const timeDiff = currentTime - Number(cachedTimestamp);
+
+    // 如果时间差小于一分钟（60000毫秒），使用缓存数据
+    if (timeDiff < 60000) {
+      visitCount.value = cachedCount;
+      console.log("Using cached visit count");
+      return true;
+    }
+  }
+
+  return false;
+};
+
+onMounted(() => {
+  // 尝试从缓存加载
+  if (!loadFromCache()) {
+    // 如果缓存不可用，从服务器获取
+    getVisitCount();
+  }
+});
+
 const markdowntext = `
 # Welcome to XBlogs
 
 **Xblogs** 是 [**XnorsCode/异或工作室**](https://github.com/xnors) 开发的一个的博客网站, 上面有 Xnors 团队成员的一些日常开发心得, 也会分享一些技术干货.
 
+> 目前已被访问 __{{ visitCount.value }}__ 次
 ## 使用
 
 本博客网站以简洁为设计理念, 界面简洁, 易于操作
@@ -28,13 +78,20 @@ const markdowntext = `
 - [Scss](https://sass-lang.com/)
 - [Vue-Router](https://router.vuejs.org/)
 
+
 `;
+
+const renderMarkdown = () => {
+  return marked(
+    markdowntext.replace("{{ visitCount.value }}", visitCount.value)
+  );
+};
 </script>
 
 <template>
   <div class="content">
     <div class="boxinside">
-      <div v-html="marked(markdowntext)"></div>
+      <div v-html="renderMarkdown(markdowntext)"></div>
     </div>
   </div>
 </template>
